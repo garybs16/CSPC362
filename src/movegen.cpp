6dce0201d3e5e0c3286c1a91d57646942b701cd5
podo03 <94269516+podo03@users.noreply.github.com>
2026-02-05 22:05:20 -0800

    Refactor pawn and knight move generation logic

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -27,14 +27,65 @@ void MoveGen::addPawnMoves(uint64_t target, int shift_, int flags_ , MoveList& m
 void MoveGen::genPawn(Board& board, MoveList& movelist)
 {
     uint64_t empty = ~board.occupancy[dualOccupancy];
-    
+    uint64_t pawns = board.piece_bitboard[P];
+    uint64_t enemy = board.occupancy[board.sideToMove ? whiteOccupancy : blackOccupancy];
     bool sideToMove = board.sideToMove;
 
-   
-    uint64_t pushOne = (board.piece_bitboard[P] << 8) & empty;
-    uint64_t pushTwo = (((board.piece_bitboard[P] << 16) & RANK_3) << 8) & empty;
-    addPawnMoves(pushOne, 8, 0, movelist);
-    addPawnMoves(pushTwo, 16, 0, movelist);
+    if (!board.sideToMove){
+      uint64_t pushOne = (pawns << 8) & empty;
+      uint64_t pushTwo = ((pushOne & RANK_3) << 8) & empty;
+      addPawnMoves(pushOne, 8, 0, movelist);
+      addPawnMoves(pushTwo, 16, 1, movelist);
+
+      uint64_t captureLeft = (pawns << 7) & ~File_H & enemy;
+      uint64_t captureRight = (pawns << 9) & ~File_A & enemy;
+      addPawnMoves(captureLeft, 7, 4, movelist);
+      addPawnMoves(captureRight, 9, 4, movelist);
+    }
+    else{
+      uint64_t pushOne = (pawns >> 8) & empty;
+      uint64_t pushTwo = ((pushOne & RANK_3) >> 8) & empty;
+      addPawnMoves(pushOne, -8, 0, movelist);
+      addPawnMoves(pushTwo, -16, 1, movelist);
+
+      uint64_t captureLeft = (pawns << 7) & ~File_H & enemy;
+      uint64_t captureRight = (pawns << 9) & ~File_A & enemy;
+      addPawnMoves(captureLeft, -7, 4, movelist);
+      addPawnMoves(captureRight, -9, 4, movelist);
+    }
   }
+
+// Knight
+void MoveGen::genKnight(Board& board, MoveList& movelist);
+{
+  uint64_t empty = ~board.occupancy[dualOccupancy];
+  uint64_t knights = board.piece_bitboard[N];
+  uint64_t allies = board.occupancy[board.sideToMove ? blackOccupancy : whiteOccupancy]
+  uint64_t enemy = board.occupancy[board.sideToMove ? whiteOccuoancy : blackOccupancy];
+
+  if (!board.sideToMove){
+    int from = popLSB(knights);
+    uint64_t position = 1ULL << from;
+    uint64_t attacks = 0ULL;
+
+    attacks |= (pos << 17) & ~File_A & ~ allies;
+    attacks |= (pos << 10) & ~(File_A | File_B) & ~allies;
+    attacks |= (pos >> 15) & ~File_A & ~allies;
+    attacks |= (pos >> 6) & ~(File_A | File_B) & ~allies;
+    attacks |= (pos << 15) & ~File_H & allies;
+    attacks |= (pos << 6) & (File_H | File_G) & ~allies;
+    attacks |= (pos >> 17) & ~File_H & ~allies;
+    attacks |= (pos >> 10) & ~(File_H | File_G) & ~allies;
+
+    uint64_t capture = attacks & enemy;
+    uint64_t notCapture = attacks & ~enemy;
+    if (capture) {
+      addMoves(capture, form, 4, movelist);
+    if (notCapture){
+      addMoves(notCapture, from, 0, movelist);
+    }
+    }
+  }
+}
 
 
