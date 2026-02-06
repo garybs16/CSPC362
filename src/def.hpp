e755717effc4e2b09f7c97d7dfb86da1355775ab
podo03 <94269516+podo03@users.noreply.github.com>
2026-02-05 20:09:30 -0800

    Add file mask constants for chess files

--- src/def.hpp
+++ src/def.hpp
@@ -12,5 +12,15 @@ namespace Masks {
     constexpr uint64_t RANK_6 = RANK_1 << 40;
     constexpr uint64_t RANK_7 = RANK_1 << 48;
     constexpr uint64_t RANK_8 = RANK_1 << 56;
+
+    // File Mask
+    constexpr uint64_t File_A = 0x010101010101010101ULL;
+    constexpr uint64_t File_B = File_A << 1;
+    constexpr uint64_t File_C = File_A << 2;
+    constexpr uint64_t File_D = File_A << 3;
+    constexpr uint64_t File_E = File_A << 4;
+    constexpr uint64_t File_F = File_A << 5;
+    constexpr uint64_t File_G = File_A << 6;
+    constexpr uint64_t File_H = File_A << 7;
 }
 #endif
